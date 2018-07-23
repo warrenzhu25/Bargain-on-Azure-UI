64128a3b8592f28a5f70e71d02d6d53a6e0bbb2f
Kenan Sun <kesu@microsoft.com>
2018-07-23 16:04:51 +0800

    updated

--- src/app/dashboard/components/dashboard-page/dashboard-page.component.ts
+++ src/app/dashboard/components/dashboard-page/dashboard-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { OrderService } from '@app/order/services/order.service';
+import { Chart } from 'angular-highcharts';
 
 @Component({
   selector: 'app-dashboard-page',
