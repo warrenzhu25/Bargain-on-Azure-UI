209a350a09d1485ade54c87d8639140fb7f69a13
weiping <wepa@microsoft.com>
2018-07-24 15:30:30 +0800

    add more fields for batch job

--- src/app/order/framework/batchjob.ts
+++ src/app/order/framework/batchjob.ts
@@ -4,7 +4,11 @@ export class BatchJob {
     public jobName: string,
     public status: string,
     public type: string,
-    public batchJobFile: string
+    public batchJobFile: string,
+    public price?: number,
+    public deadline?: Date,
+    public suggestedPrice?: number,
+    public suggestedDeadline?: Date
   ) { }
 
 }
