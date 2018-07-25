33d1a612a77de8ba5494b4f491f8bf0c38e042ad
weiping <wepa@microsoft.com>
2018-07-25 11:25:18 +0800

    move urls into constants

---
+++ src/app/constants/Url.ts
@@ -0,0 +1,6 @@
+export class ServiceUrls {
+  private static readonly SERVICE_ROOT_URL = "https://bargain-on-azure.azurewebsites.net";
+
+  public static readonly JOB_LIST_URL = ServiceUrls.SERVICE_ROOT_URL + "/jobs";
+  public static readonly SUBMIT_JOB_URL = ServiceUrls.SERVICE_ROOT_URL + "/jobs";
+}
--- src/app/order/services/order.service.ts
+++ src/app/order/services/order.service.ts
@@ -1,18 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BatchJob } from '@app/order/framework/batchjob';
+import * as Urls from '@app/constants/Url';
 
 @Injectable()
 export class OrderService {
   constructor(private http: HttpClient) { }
-  jobListUrl = "https://bargain-on-azure.azurewebsites.net/jobs";
-  submitJobUrl = "https://bargain-on-azure.azurewebsites.net/jobs";
-
   create(batchJob: BatchJob) {
-    return this.http.post(this.submitJobUrl, batchJob);
+    return this.http.post(Urls.ServiceUrls.SUBMIT_JOB_URL, batchJob);
   }
 
   getBatchJobList() {
-    return this.http.get(this.jobListUrl);
+    return this.http.get(Urls.ServiceUrls.JOB_LIST_URL);
   }
 }
